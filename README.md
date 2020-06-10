86a29dfa61a59a8d4f6fb723828e15b655d8d27c
Ozan Eğitmen <ozan@egitmen.net>
2020-06-11 00:13:13 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # ieee-election
+
+## Setup
+
+1) Install poetry: https://python-poetry.org/docs/#installation
+2) Run `poetry install` in root folder
+3) Install node (with npm): https://nodejs.org/en/download/
+4) Run `npm install` in `/frontend`
+
+## Development
+
+
+Run this in root folder
+```
+poetry run uvicorn app.main:app --reload
+```
+
+Then run this in `/fontend`:
+```
+npm run dev
+```
+
+## Deployment
+
---
+++ frontend/build.js
@@ -0,0 +1,24 @@
+/* eslint-env node */
+
+const fs = require("fs").promises;
+const Bundler = require("parcel");
+
+(async () => {
+    // Clear dist
+    try {
+        await fs.access("./dist");
+        for (const filename of await fs.readdir("./dist")) {
+            await fs.unlink(`./dist/${filename}`);
+        }
+    } catch (error) {
+        console.info("dist doesn't exist, skipping");
+    }
+
+    // Run parcel bundler
+    await new Bundler("./src/index.html", {
+        production: true,
+        contentHash: false,
+        sourceMaps: false,
+        publicURL: "./"
+    }).bundle();
+})().catch(e => console.error(e));
